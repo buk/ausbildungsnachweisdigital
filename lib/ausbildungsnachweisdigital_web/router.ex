744be77016e548759b30a93afd91033b9350d0a6
Sebastian Bachmann <github@sebastianbachmann.de>
2023-06-15 06:37:18 +0200

    chore: User Auth changes
    This changes were somehow not committed

--- lib/ausbildungsnachweisdigital_web/router.ex
+++ lib/ausbildungsnachweisdigital_web/router.ex
@@ -4,23 +4,23 @@ defmodule AusbildungsnachweisdigitalWeb.Router do
   import AusbildungsnachweisdigitalWeb.UserAuth
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, html: {AusbildungsnachweisdigitalWeb.Layouts, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-    plug :fetch_current_user
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, html: {AusbildungsnachweisdigitalWeb.Layouts, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
+    plug(:fetch_current_user)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", AusbildungsnachweisdigitalWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :home
+    get("/", PageController, :home)
   end
 
   # Other scopes may use custom stacks.
@@ -38,55 +38,55 @@ defmodule AusbildungsnachweisdigitalWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      live_dashboard "/dashboard", metrics: AusbildungsnachweisdigitalWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      live_dashboard("/dashboard", metrics: AusbildungsnachweisdigitalWeb.Telemetry)
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 
   ## Authentication routes
 
   scope "/", AusbildungsnachweisdigitalWeb do
-    pipe_through [:browser, :redirect_if_user_is_authenticated]
+    pipe_through([:browser, :redirect_if_user_is_authenticated])
 
     live_session :redirect_if_user_is_authenticated,
       on_mount: [{AusbildungsnachweisdigitalWeb.UserAuth, :redirect_if_user_is_authenticated}] do
-      live "/users/register", UserRegistrationLive, :new
-      live "/users/log_in", UserLoginLive, :new
-      live "/users/reset_password", UserForgotPasswordLive, :new
-      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+      live("/users/register", UserRegistrationLive, :new)
+      live("/users/log_in", UserLoginLive, :new)
+      live("/users/reset_password", UserForgotPasswordLive, :new)
+      live("/users/reset_password/:token", UserResetPasswordLive, :edit)
     end
 
-    post "/users/log_in", UserSessionController, :create
+    post("/users/log_in", UserSessionController, :create)
   end
 
   scope "/", AusbildungsnachweisdigitalWeb do
-    pipe_through [:browser, :require_authenticated_user]
+    pipe_through([:browser, :require_authenticated_user])
 
     live_session :require_authenticated_user,
       on_mount: [{AusbildungsnachweisdigitalWeb.UserAuth, :ensure_authenticated}] do
-      live "/users/settings", UserSettingsLive, :edit
-      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+      live("/users/settings", UserSettingsLive, :edit)
+      live("/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email)
 
-      live "/reports", ReportLive.Index, :index
-      live "/reports/new", ReportLive.Index, :new
-      live "/reports/:id/edit", ReportLive.Index, :edit
+      live("/reports", ReportLive.Index, :index)
+      live("/reports/new", ReportLive.Index, :new)
+      live("/reports/:id/edit", ReportLive.Index, :edit)
 
-      live "/reports/:id", ReportLive.Show, :show
-      live "/reports/:id/show/edit", ReportLive.Show, :edit
+      live("/reports/:id", ReportLive.Show, :show)
+      live("/reports/:id/show/edit", ReportLive.Show, :edit)
     end
   end
 
   scope "/", AusbildungsnachweisdigitalWeb do
-    pipe_through [:browser]
+    pipe_through([:browser])
 
-    delete "/users/log_out", UserSessionController, :delete
+    delete("/users/log_out", UserSessionController, :delete)
 
     live_session :current_user,
       on_mount: [{AusbildungsnachweisdigitalWeb.UserAuth, :mount_current_user}] do
-      live "/users/confirm/:token", UserConfirmationLive, :edit
-      live "/users/confirm", UserConfirmationInstructionsLive, :new
+      live("/users/confirm/:token", UserConfirmationLive, :edit)
+      live("/users/confirm", UserConfirmationInstructionsLive, :new)
     end
   end
 end
